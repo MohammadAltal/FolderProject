8273eefe89ea31fba82897c632bd41957c5f221f
Mohammad Altal <eng.hamodaaltal@gmail.com>
2017-09-06 23:59:33 +0300

    Version 2

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
 		$desk = $_POST['desk'];
 		$name = $_POST['name'];
@@ -11,18 +13,78 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 
 		if(file_exists($dir) != 1) {
+
+			if($type == 0 ) {
 				mkdir($dir);
 				mkdir("$dir/css");
 				mkdir("$dir/js");
 				mkdir("$dir/fonts");
 				mkdir("$dir/img");
 
-				$file = fopen("$dir/index$type", "w") or die("Unable to open file!");
+				$file = fopen("$dir/index.php", "w") or die("Unable to open file!");
 				$css = fopen("$dir/css/style.css", "w") or die("Unable to open file!");
 				$js = fopen("$dir/js/backend.js", "w") or die("Unable to open file!");
 				fclose($js);
 				fclose($css);
 				fclose($file);
+			}
+			elseif($type == 2){
+				mkdir($dir);
+				mkdir("$dir/css");
+				mkdir("$dir/js");
+				mkdir("$dir/fonts");
+				mkdir("$dir/img");
+
+				$file = fopen("$dir/index.html", "w") or die("Unable to open file!");
+				$css = fopen("$dir/css/style.css", "w") or die("Unable to open file!");
+				$js = fopen("$dir/js/backend.js", "w") or die("Unable to open file!");
+				fclose($js);
+				fclose($css);
+				fclose($file);
+
+			}
+			elseif($type == 1){
+				mkdir($dir);
+				mkdir("$dir/layout");
+				mkdir("$dir/includes");
+				mkdir("$dir/data");
+				mkdir("$dir/docs");
+
+
+				mkdir("$dir/layout/css");
+				$css = fopen("$dir/layout/css/style.css", "w") or die("Unable to open file!");
+				fclose($css);
+				mkdir("$dir/layout/js");
+				$js = fopen("$dir/layout/js/backend.js", "w") or die("Unable to open file!");
+				fclose($js);
+				mkdir("$dir/layout/images");
+				mkdir("$dir/layout/fonts");
+
+
+				mkdir("$dir/includes/templates");
+				$header = fopen("$dir/includes/templates/header.php", "w") or die("Unable to open file!");
+				fclose($header);
+				$navbar = fopen("$dir/includes/templates/navbar.php", "w") or die("Unable to open file!");
+				fclose($navbar);
+				$footer = fopen("$dir/includes/templates/footer.php", "w") or die("Unable to open file!");
+				fclose($footer);
+				mkdir("$dir/includes/functions");
+				$functions = fopen("$dir/includes/functions/functions.php", "w") or die("Unable to open file!");
+				fclose($functions);
+				mkdir("$dir/includes/languages");
+				$languages = fopen("$dir/includes/languages/languages.php", "w") or die("Unable to open file!");
+				fclose($languages);
+				mkdir("$dir/includes/libs");
+
+				mkdir("$dir/data/upload");
+				mkdir("$dir/data/logs");
+
+				$file = fopen("$dir/index.php", "w") or die("Unable to open file!");
+				fclose($file);
+				$init = fopen("$dir/init.php", "w") or die("Unable to open file!");
+				fclose($init);
+
+			}
 		}
 
 
@@ -32,6 +94,10 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 
 ?>
+
+
+
+
 
 <!DOCTYPE html>
 <html>
@@ -54,8 +120,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 					<option value="F:/">F/..</option>
 				</select>
 				<select name="type" class="form-control">
-					<option value=".php">.php</option>
-					<option value=".html">.html</option>
+					<option value="0">PHP</option>
+					<option value="1">PHP Full Project</option>
+					<option value="2">HTML</option>
 				</select>
 				<input type="text" name="name"  class="form-control" placeholder="Name Of Folder">
 				<input type="submit" value="Create" class="btn btn-primary btn-block">
